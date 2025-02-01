506f2c0f3b22f2ecc2bedd3e12d7c6883bf03b9b
Isabelle Schafhauser <isabelle.schafhauser@hedinit.com>
2025-02-01 13:09:30 +0100

    :construction: Add initial plugin logic

--- Flow.Launcher.Plugin.Gitmoji/Main.cs
+++ Flow.Launcher.Plugin.Gitmoji/Main.cs
@@ -1,21 +1,53 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
 using Flow.Launcher.Plugin;
 
-namespace Flow.Launcher.Plugin.Gitmoji
+namespace Flow.Launcher.Plugin.Gitmoji;
+
+public class Gitmoji : IAsyncPlugin
 {
-    public class Gitmoji : IPlugin
+    private const string _apiUrl = "https://gitmoji.dev/api/gitmojis";
+    private PluginInitContext _context;
+
+    public async Task<List<Result>> QueryAsync(Query query, CancellationToken token)
     {
-        private PluginInitContext _context;
+        var response = await _context.API.HttpGetStringAsync(_apiUrl + Uri.EscapeDataString(query.Search), token);
+        var json = JsonSerializer.Deserialize<GitmojiResponse>(response);
 
-        public void Init(PluginInitContext context)
-        {
-            _context = context;
-        }
-
-        public List<Result> Query(Query query)
-        {
-            return new List<Result>();
-        }
+        return json.Gitmojis.Select(g => new Result {
+            Title = $"{g.Code} {g.Name}",
+            SubTitle = g.Description,
+            Action = _ => {
+                _context.API.CopyToClipboard(g.Code);
+                return true;
+            },
+            IcoPath = "app.png";
+        }).ToList();
     }
+
+    public async Task InitAsync(PluginInitContext context)
+    {
+        _context = context;
+    }
+}
+
+public class GitmojiModel
+{
+    public string Name { get; set; }
+    public string Code { get; set; }
+    public string Emoji { get; set; }
+    public string Description { get; set; }
+}
+
+public class GitmojiResponse
+{
+    public List<GitmojiModel> Gitmojis { get; set; }
 }
